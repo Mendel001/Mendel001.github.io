ed2ab22b7d450a2a67ad0cebe7db3522592fae0e
Mendel001 <mdmendelsondorlus@gmail.com>
2023-06-27 16:14:06 +0000

    saving image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,9 +20,11 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
+  applyFilter(reddify);
   
-
+  applyFilter(reddify);
+  applyFilter(decreaseBlue);
+  applyFilter(increaseGreenByBlue);
   // do not change the below line of code
   render($("#display"), image);
 }
@@ -32,18 +34,59 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
+function applyFilter(filterFunction) {
+  for (var i = 0; i < image.length; i++) {
+
+    for (var j = 0; j < image[i].length; j++) {
+      var rgbString = image[i][j];
+
+      var rgbNumbers = rgbStringToArray(rgbString);
+      //rgbNumbers[RED] = 255;
+      filterFunction(rgbNumbers);
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][j] = rgbString;
+    }
+  }
+}
 
 
 // TODO 7: Create the applyFilterNoBackground function
+function applyFilterNoBackground(filterFunction) {
+  var backgroundColor = image[150];
 
+  for (var i = 0; i < image.length; i++) {
+
+    for (var j = 0; j < image[i].length; j++) {
+      
+      var rgbString = image[i][j];
+      if (image[150] !== backgroundColor) {
+      var rgbNumbers = rgbStringToArray(rgbString);
+      //rgbNumbers[RED] = 255;
+      filterFunction(rgbNumbers);
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][j] = rgbString;
+      }
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds (num){
+  return Math.min(255, Math.max(0, num));
+}
 
 // TODO 3: Create reddify function
-
+function reddify (arr) {
+arr[RED] = 200;
+}
 
 // TODO 6: Create more filter functions
+function decreaseBlue (ar) {
+  ar[BLUE] = keepInBounds(ar[BLUE] - 50)
+}
 
+function increaseGreenByBlue (ay) {
+  ay[GREEN] = keepInBounds(ay[BLUE] + ay[GREEN])
+}
 
 // CHALLENGE code goes below here
